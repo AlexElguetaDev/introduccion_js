63c44f7e7177c1de850611b3cadcb9fb3c313ca6
Alex Damian Elgueta Rivas <alexelgueta.developer@gmail.com>
2022-10-12 12:53:44 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+let alexElguetaDev = [
+    personalInformation ={
+        nombre   : "alex",
+        apellido : "elgueta",
+        edad     : 26,
+        developer: true,
+    },
+    birthDay            = {
+        day      : 7,
+        month    : "Junio",
+        year     : 1996
+    },
+    favoriteBook        = {
+        title    : "Fuego ysangre",
+        autor    : "George R. R. Martin",
+        date     : 2018,
+        url      : "https://es.wikipedia.org/wiki/Fuego_y_sangre_(novela)",
+    },
+    hobby               = ["baloncesto", "musica"],
+    function myFavouriteActivity(){
+        return "Aprender sobre Js"
+    }
+]
